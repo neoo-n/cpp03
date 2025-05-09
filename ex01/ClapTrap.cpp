178d00b02ab9ff4619e13ef2e7a0ea15d3f730b9
neoo-n <dana.vauthey@gmail.com>
2025-05-09 15:44:46 +0200

    ex01 start

---
+++ ex01/ClapTrap.cpp
@@ -0,0 +1,139 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dvauthey <dvauthey@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/08 14:05:40 by dvauthey          #+#    #+#             */
+/*   Updated: 2025/05/09 14:45:24 by dvauthey         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+// ------------------------ CONSTRUCTOR & DESTRUCTOR ---------------------------
+
+ClapTrap::ClapTrap(void)
+: _name(0), _hit_pt(10), _energy_pt(10), _attack_dmg(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap &other)
+: _name(other._name), _hit_pt(other._hit_pt), _energy_pt(other._energy_pt), _attack_dmg(other._attack_dmg)
+{
+	std::cout << "Copy constructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap(const std::string name)
+: _name(name), _hit_pt(10), _energy_pt(10), _attack_dmg(0)
+{
+	std::cout << "Constructor called" << std::endl;
+}
+
+ClapTrap	&ClapTrap::operator=(const ClapTrap &obj)
+{
+	this->_name = obj._name;
+	this->_hit_pt = obj._hit_pt;
+	this->_energy_pt = obj._energy_pt;
+	this->_attack_dmg = obj._attack_dmg;
+	return (*this);
+}
+
+ClapTrap::~ClapTrap(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+// ------------------------------- GET AND SET ---------------------------------
+
+std::string		ClapTrap::getName(void) const
+{
+	return (_name);
+}
+
+unsigned int	ClapTrap::getHit(void) const
+{
+	return (_hit_pt);
+}
+
+unsigned int	ClapTrap::getEnergy(void) const
+{
+	return (_energy_pt);
+}
+
+unsigned int	ClapTrap::getAttack(void) const
+{
+	return (_attack_dmg);
+}
+
+void	ClapTrap::setName(std::string name)
+{
+	_name = name;
+}
+
+void	ClapTrap::setHit(unsigned int hit)
+{
+	_hit_pt = hit;
+}
+void	ClapTrap::setEnergy(unsigned int energy)
+{
+	_energy_pt = energy;
+}
+
+void	ClapTrap::setAttack(unsigned int attack)
+{
+	_attack_dmg = attack;
+}
+
+// -------------------------------- METHODS ------------------------------------
+
+void	ClapTrap::attack(const std::string &target)
+{
+	if (this->_energy_pt == 0)
+	{
+		std::cout << this->_name << " has no energy point left, cannot attack" << std::endl;
+		return ;
+	}
+	if (this->_hit_pt == 0)
+	{
+		std::cout << this->_name << " has no hit point left, cannot attack" << std::endl;
+		return ;
+	}
+	this->_energy_pt--;
+	std::cout << "ClapTrap " << this->_name << " attacks " << target;
+	std::cout << ", causing " << this->_attack_dmg << " points of damage!" << std::endl;
+}
+
+void	ClapTrap::takeDamage(unsigned int amount)
+{
+	if (this->_hit_pt == 0)
+	{
+		std::cout << this->_name << " has already no hit point left" << std::endl;
+		return ;
+	}
+	if (this->_hit_pt < amount)
+	{
+		this->_hit_pt = 0;
+		return ;
+	}
+	this->_hit_pt -= amount;
+	
+}
+
+void	ClapTrap::beRepaired(unsigned int amount)
+{
+	if (this->_energy_pt == 0)
+	{
+		std::cout << this->_name << " has no energy point left, cannot repair itself" << std::endl;
+		return ;
+	}
+	if (this->_hit_pt == 0)
+	{
+		std::cout << this->_name << " has no hit point left, cannot repair itself" << std::endl;
+		return ;
+	}
+	this->_energy_pt--;
+	this->_hit_pt += amount;
+}
---
+++ ex01/ClapTrap.hpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dvauthey <dvauthey@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/06 14:34:44 by dvauthey          #+#    #+#             */
+/*   Updated: 2025/05/09 11:27:20 by dvauthey         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	CLAP_TRAP_HPP
+# define CLAP_TRAP_HPP
+
+# include <iostream>
+
+class ClapTrap
+{
+	private:
+		std::string 	_name;
+		unsigned int	_hit_pt;
+		unsigned int	_energy_pt;
+		unsigned int	_attack_dmg;
+
+	public:
+		ClapTrap(void);
+		ClapTrap(const ClapTrap &other);
+		ClapTrap(const std::string name);
+		ClapTrap &operator=(const ClapTrap &obj);
+		~ClapTrap();
+
+		std::string		getName(void) const;
+		unsigned int	getHit(void) const;
+		unsigned int	getEnergy(void) const;
+		unsigned int	getAttack(void) const;
+
+		void	setName(std::string name);
+		void	setHit(unsigned int hit);
+		void	setEnergy(unsigned int energy);
+		void	setAttack(unsigned int attack);
+
+		void	attack(const std::string &target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+};
+
+
+#endif
